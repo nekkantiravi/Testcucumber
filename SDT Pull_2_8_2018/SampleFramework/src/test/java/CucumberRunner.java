5758f6664d4e91f7470477d23dd42a9b24793690
Ravi Nekkanti <rnekkanti@osius.com>
2018-09-17 15:44:23 +0530

    SDT framework

---
+++ SDT Pull_2_8_2018/SampleFramework/src/test/java/CucumberRunner.java
@@ -0,0 +1,7 @@
+/**
+ * Created by rnekkanti on 2/20/2018.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(")
+public class CucumberRunner {
+}
---
+++ src/main/java/com/Cucumberframework/Example1.java
@@ -0,0 +1,24 @@
+package com.Cucumberframework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Example1 {
+	WebDriver driver;
+	//public static void main(String[] args) {
+	public void text() {
+	
+		 driver.findElement(By.xpath("")).sendKeys("");
+		 
+	}
+	
+	}
+
+	 
+	
+	
+	
+     
+//}
